33007dad4baaacec3198a33ed944d2db66261b70
minhviet21 <128829837+minhviet21@users.noreply.github.com>
2024-06-15 11:00:44 +0700

    Delete a.py

--- a.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model_embedding = SentenceTransformer('bkai-foundation-models/vietnamese-bi-encoder')
-
-def embed_question(question):
-    return model_embedding.encode(question)
