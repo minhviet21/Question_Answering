ab38909d755d94e7c1bd23d034486a6c53379f6b
minhviet21 <128829837+minhviet21@users.noreply.github.com>
2024-06-16 15:19:42 +0700

    Update search.py

--- answer_question/search.py
+++ answer_question/search.py
@@ -27,3 +27,40 @@ def hybrid_search(query, vector):
                               vector = vector 
                               ).with_limit(1).with_additional(['certainty']).do()
     return result['data']['Get']['Test1'][0]['content']
+
+def vector_to_BM25_search(vector, question):
+    nearVector = {"vector": vector}
+    top_results = client.query.get("test1", ["law_id", "article_id", "title", "index", "content", "_additional { id }"]
+                        ).with_near_vector(nearVector).with_limit(10).with_additional(['certainty']).do()
+    top_results = top_results['data']['Get']['Test1']
+    
+    id_list = [obj["_additional"]["id"] for obj in top_results]
+    
+    where_filter = {"path": "id", "operator": "ContainsAny", "valueStringArray": id_list}
+    
+    result = client.query.get("test1", ["law_id", "article_id", "title", "index", "content", "_additional { id }"]
+                        ).with_where(where_filter
+                        ).with_bm25(
+                          query=question,
+                          properties=["content"]
+                        ).with_limit(1).with_additional(['certainty']).do()
+    
+    return result['data']['Get']['Test1'][0]['content']
+
+def BM25_to_vector_search(vector, question):
+    top_results = client.query.get("test1", ["law_id", "article_id", "title", "index", "content", "_additional { id }"]
+                        ).with_bm25(
+                          query=question,
+                          properties=["content"]
+                        ).with_limit(10).with_additional(['certainty']).do()
+    top_results = top_results['data']['Get']['Test1']
+    
+    id_list = [obj["_additional"]["id"] for obj in top_results]
+    
+    where_filter = {"path": "id", "operator": "ContainsAny", "valueStringArray": id_list}
+    
+    nearVector = {"vector": vector}
+    result = client.query.get("test1", ["law_id", "article_id", "title", "index", "content", "_additional { id }"]
+                        ).with_where(where_filter
+                        ).with_near_vector(nearVector).with_limit(1).with_additional(['certainty']).do()
+    return result['data']['Get']['Test1'][0]['content']
