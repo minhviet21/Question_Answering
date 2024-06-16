43f0fda438170e4457d44e0f9866c6231ccbe038
minhviet21 <128829837+minhviet21@users.noreply.github.com>
2024-06-16 20:33:57 +0700

    Update process.py

--- answer_question/process.py
+++ answer_question/process.py
@@ -4,7 +4,7 @@ def tokenize(text):
     return ViTokenizer.tokenize(text)
 
 def remove_stopwords(text):
-    filepath = 'Question_Answering\\tokenized_stopwords.txt'
+    filepath = 'tokenized_stopwords.txt'
     with open(filepath, 'r', encoding='utf-8') as file:
         stopwords = set(line.strip() for line in file)
     return ' '.join([word for word in text.split() if word.lower() not in stopwords])
