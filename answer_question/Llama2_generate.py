724959704e3a80b9c9d722a0cd90a0d329d949ff
minhviet21 <128829837+minhviet21@users.noreply.github.com>
2024-06-22 09:00:13 +0700

    Update Llama2_generate.py

--- answer_question/Llama2_generate.py
+++ answer_question/Llama2_generate.py
@@ -8,20 +8,20 @@ path = "meta-llama/Llama-2-7b-chat-hf"
 tokenizer = AutoTokenizer.from_pretrained(path, cache_dir="/data/yash/base_models", token = token)
 model = AutoModelForCausalLM.from_pretrained(path, device_map='auto', token = token)
 
-def get_difference(input ,response):
-    return response[len(input)-1:]
+def get_answer(response):
+    if "ANSWER:" in response:
+        start_index = response.index("ANSWER:")
+        return response[start_index+8:]
+    else:
+        return response
 
-def generate(text, max_new_tokens=100, temperature=0.7, top_k=50, top_p=0.9):
-    with torch.no_grad():
-        inputs = tokenizer(text, return_tensors="pt").to(device)
-        outputs = model.generate(
-            **inputs,
-            max_new_tokens=max_new_tokens,
-            temperature=temperature,
-            top_k=top_k,
-            top_p=top_p,
-            eos_token_id=tokenizer.eos_token_id,
-            early_stopping=True
-        )
-        response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-    return get_difference(text, response)
+def generate(question, context):
+    prompt = f"""
+CONTEXT: {en_context}
+
+QUESTION: {en_question}
+"""
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+    outputs = model.generate(input_ids, max_new_tokens=200)
+    response = tokenizer.decode(outputs[0],skip_special_tokens=True)
+    return get_answer(response)
