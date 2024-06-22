848d0e8fd0c1b9123bb7bc87ea745060d6fa2cc5
minhviet21 <128829837+minhviet21@users.noreply.github.com>
2024-06-22 14:04:04 +0700

    Update T5_generate.py

--- answer_question/T5_generate.py
+++ answer_question/T5_generate.py
@@ -1,6 +1,8 @@
 import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
 path = "google/flan-t5-xl"
 tokenizer = T5Tokenizer.from_pretrained(path)
 model = T5ForConditionalGeneration.from_pretrained(path, device_map="auto")
