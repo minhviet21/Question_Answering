5bff81365e9f148834e9b071bf0b1b4b37763c5f
minhviet21 <128829837+minhviet21@users.noreply.github.com>
2024-06-22 13:55:16 +0700

    Update T5_generate.py

--- answer_question/T5_generate.py
+++ answer_question/T5_generate.py
@@ -1,13 +1,17 @@
 import torch
 from transformers import T5Tokenizer, T5ForConditionalGeneration
 
-path = "google/flan-t5-large"
-tokenizer = T5Tokenizer.from_pretrained(T5_path)
-model = T5ForConditionalGeneration.from_pretrained(T5_path, device_map="auto")
+path = "google/flan-t5-xl"
+tokenizer = T5Tokenizer.from_pretrained(path)
+model = T5ForConditionalGeneration.from_pretrained(path, device_map="auto")
 
-def generate(text):
-    with torch.no_grad():
-        inputs = tokenizer(text, return_tensors="pt").to(device)
-        outputs = model.generate(**inputs, max_new_tokens=100, temperature = 0.001, early_stopping = True)
-        response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+def generate(question, context):
+    prompt = f"""
+CONTEXT: {context}
+
+QUESTION: {question}
+"""
+    input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+    outputs = model.generate(input_ids, max_new_tokens=200)
+    response = tokenizer.decode(outputs[0],skip_special_tokens=True)
     return response
